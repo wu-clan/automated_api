ea5fac94d1231935eec0e3020ffa674b523a73ca
wu <jianhengwu0407@gmail.com>
2021-07-20 18:47:53 +0800

    small update

--- DemoAPI/config/setting.py
+++ DemoAPI/config/setting.py
@@ -2,18 +2,18 @@
 # _*_ coding:utf-8 _*_
 
 
-import os,sys
+import os, sys
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(BASE_DIR)
 
 # 配置文件
-TEST_CONFIG =  os.path.join(BASE_DIR,"database","config.ini")
+TEST_CONFIG = os.path.join(BASE_DIR, "database", "config.ini")
 # 测试用例模板文件
-SOURCE_FILE = os.path.join(BASE_DIR,"database","DemoAPITestCase.xlsx")
+SOURCE_FILE = os.path.join(BASE_DIR, "database", "DemoAPITestCase.xlsx")
 # excel测试用例结果文件
-TARGET_FILE = os.path.join(BASE_DIR,"report","excelReport","DemoAPITestCase.xlsx")
+TARGET_FILE = os.path.join(BASE_DIR, "report", "excelReport", "DemoAPITestCase.xlsx")
 # 测试用例报告
-TEST_REPORT = os.path.join(BASE_DIR,"report")
+TEST_REPORT = os.path.join(BASE_DIR, "report")
 # 测试用例程序文件
-TEST_CASE = os.path.join(BASE_DIR,"testcase")
-
+TEST_CASE = os.path.join(BASE_DIR, "testcase")
--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 - python3 + selenium3 + unittest + ddt + requests
 
 ## 目录结构介绍
-- config/: 文件路径配置
-- database/: 测试用例模板文件及数据库和发送邮箱配置文件
-- db_fixture/: 初始化接口测试数据
-- lib/: 程序核心模块。包含有excel解析读写、发送邮箱、发送请求、生成最新测试报告文件
-- package/: 存放第三方库包。如HTMLTestRunner，用于生成HTML格式测试报告
+- config/: 配置
+- data/: 测试用例模板文件
+- db/: 初始化接口测试数据
+- models/: 程序核心模块。包含有excel解析读写、发送邮箱、发送请求、生成最新测试报告文件
+- packages/: 存放第三方库包。如HTMLTestRunner，用于生成HTML格式测试报告
 - report/: 生成接口自动化测试报告
 - testcase/: 用于编写接口自动化测试用例
 - run_demo.py: 执行所有接口测试用例的主程序
