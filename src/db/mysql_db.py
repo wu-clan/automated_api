dd30d77ebdb624c2d094cc26da645d3d0ecc011b
wu <jianhengwu0407@gmail.com>
2022-03-28 00:06:33 +0800

    添加数据线程池

--- src/db/mysql_db.py
+++ src/db/mysql_db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 import pymysql
-from pymysql.err import OperationalError
+from dbutils.pooled_db import PooledDB
 
 from src.common.log import log
 from src.core.conf import settings
@@ -11,19 +11,22 @@ class DB:
 
     def __init__(self):
         try:
-            self.conn = pymysql.connect(
+            self.conn = PooledDB(
+                pymysql,
+                maxconnections=15,
+                blocking=True,  # 防止连接过多报错
                 host=settings.DB_HOST,
                 port=settings.DB_PORT,
                 user=settings.DB_USER,
                 password=settings.DB_PASSWORD,
                 database=settings.DB_DATABASE,
-                charset=settings.DB_CHARSET
-            )
-        except OperationalError as e:
-            log.error(f'数据库连接失败\n {e}')
+                charset=settings.DB_CHARSET,
+            ).connection()
+        except BaseException as e:
+            log.error(f'数据库连接失败 \n {e}')
         self.cursor = self.conn.cursor()
 
-    def ect(self, sql):
+    def execute(self, sql):
         """
         数据库操作执行
         :return:
@@ -33,13 +36,16 @@ class DB:
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
-            log.error(f'执行 {sql} 时发生错误\n {e}')
+            log.error(f'执行 {sql} 失败 \n {e}')
+        else:
+            self.close()
 
     def close(self):
         """
         关闭数据库
         :return:
         """
+        self.cursor.close()
         self.conn.close()
 
 
